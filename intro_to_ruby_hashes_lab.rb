b80d9eaa933fb06411e0028cbafa0103b8c60879
Greem <orangenewyorkcreativeagency@gmail.com>
2020-04-30 03:41:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,7 +10,7 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  new_hash = {:pieces => {}}
+  new_hash = {:railroads => {:pieces => {}}}
   new_hash
 end
 
